9d484aebf0c4ba2988ba1db8b26ca0cdc65667f7
Michael Geers <michael.geers@neuland-bfi.de>
2009-11-12 13:39:35 +0100

    making it work

--- unify.rb
+++ unify.rb
@@ -7,33 +7,47 @@ def main
   matcher = Regexp.new REGEX_BEGIN.gsub(/XXX/,"(.*?\\.html)");
 
   files.each do |file|
-    puts
-    puts ">> #{file}"
-    
-    text = File.read(file)
-    
-    File.read(file).scan pattern do |p|
-      template = p[0]
-      
-      target_content = template_content(template)
-      if target_content
-        if has_begin_and_end_tag(text, template)
-          source_content = content(text, template)
-          text.gsub!(source_content, target_content)
-          puts "beginning: #{template}"
-          puts "end: #{template}"
-        end
-      end
-    end
-    puts text;
+    print "#{file}\t updating:"
+    update_file file
+    print "\n"
   end
+end
+
+def update_file file
+  text = File.read(file)
+  text.clone.scan(scan_pattern) do |p|
+    text = update_part(text,p[0])
+  end
+  File.open(file, "w") { |f| f << text }
+end
+
+def update_part text, template
+  # getting fragment
+  target_content = template_content(template)
+  if target_content.nil?
+    puts "\nerror: '#{template}' not found"
+    return text
+  end
+  
+  # check if begin and end are present and in correct order
+  if !has_begin_and_end_tag(text, template)
+    puts "\nerror: missing '#{end_pattern(template)}'"
+    return text
+  end
+  
+  source_content = content(text, template)
+  if source_content != target_content
+    print " _#{template}"            
+    text = replace(text,template,target_content)
+  end
+  text
 end
 
 def files
   Dir.entries(".").select {|f| f =~ /^[^_].*\.html/}
 end
 
-def pattern
+def scan_pattern
   Regexp.new REGEX_BEGIN.gsub(/XXX/,"(.*?\\.html)");
 end
 
@@ -45,8 +59,8 @@ def end_pattern template
   REGEX_END.gsub(/XXX/,template)
 end
 
-def content_pattern template
-  Regexp.new(begin_pattern(template)+"(.*?)"+end_pattern(template),Regexp::MULTILINE)
+def surround_with_patterns template, inner
+  begin_pattern(template)+inner+end_pattern(template)
 end
 
 def has_begin_and_end_tag text, template
@@ -56,11 +70,18 @@ def has_begin_and_end_tag text, template
 end
 
 def content text, template
-  text.match(content_pattern(template))[1].strip
+  p = Regexp.new(surround_with_patterns(template,"(.*?)"),Regexp::MULTILINE)
+  text.match(p)[1].strip
 end
 
 def template_content template
-  File.read("_#{template}")
+  File.read "_#{template}" if File.exists? "_#{template}"
+end
+
+def replace text, template, content
+  p = Regexp.new(surround_with_patterns(template,"(.*?)"),Regexp::MULTILINE)
+  r = surround_with_patterns(template,"\n#{content}\n")
+  text.gsub(p, r)
 end
 
 main
